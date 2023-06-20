c2b14956cd906ce919999172ebe3d3679271513d
Alejandro Arroyo <78103374+alesitohp@users.noreply.github.com>
2023-06-20 20:31:09 +0200

    Main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.white));
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'botones',
+      routes: {
+        'basico': (BuildContext context) => BasicoPage(),
+      },
+    );
+  }
+}
